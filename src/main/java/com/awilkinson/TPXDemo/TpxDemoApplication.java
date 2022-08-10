2fb95807d51015a75fdcf32a9ff3cd13154f692e
Andrew Wilkinson <abwilkinson@gmail.com>
2022-08-10 17:39:53 +0100

    initial project structure

---
+++ src/main/java/com/awilkinson/TPXDemo/TpxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.awilkinson.TPXDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpxDemoApplication.class, args);
+	}
+
+}
